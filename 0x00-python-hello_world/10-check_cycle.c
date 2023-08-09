d9f8fddc4fce32e9f804c4d762d60e450b42113b
Salma_Salah <salma.s.kabel@gmail.com>
2023-08-09 06:40:57 -0700

    answer

--- 0x00-python-hello_world/10-check_cycle.c
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -8,11 +8,16 @@ int check_cycle(listint_t *list)
 
 	node = list->next;
 	ptr = list;
-	while (node != NULL)
+	while (node != NULL && node->next != NULL)
 	{
-		if (node->next == ptr)
-			return (1);
-		node = node->next;
+                while (ptr != node)
+                {
+                        if (node->next == ptr)
+                                return (1);
+                        ptr = ptr->next;
+                }
+                ptr = list;
+                node = node->next;
 	}
 	return (0);
 }
