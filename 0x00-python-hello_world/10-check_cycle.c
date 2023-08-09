223e3b75017cba2578129928904c5c6a79fb2f52
Salma_Salah <salma.s.kabel@gmail.com>
2023-08-09 06:27:08 -0700

    answer

---
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -0,0 +1,23 @@
+#include "lists.h"
+
+
+
+int check_cycle(listint_t *list)
+{
+	listint_t *ptr, *node;
+
+	node = list->next;
+	ptr = list;
+	while (node != NULL)
+	{
+		while (ptr != node)
+		{
+			if (node->next == ptr)
+				return (1);
+			ptr = ptr->next;
+		}
+		ptr = list;
+		node = node->next;
+	}
+	return (0);
+}
