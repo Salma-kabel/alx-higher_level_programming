d6c91af6d8edbbcce8270e95bf6024925b5bcb7d
Salma_Salah <salma.s.kabel@gmail.com>
2023-08-22 08:06:51 -0700

    answer2

--- 0x03-python-data_structures/100-print_python_list_info.c
+++ 0x03-python-data_structures/100-print_python_list_info.c
@@ -11,6 +11,6 @@ void print_python_list_info(PyObject *p)
 
 	printf("[*] Size of the Python list = %d\n", len);
 	printf("[*] Allocated = %d\n", object->allocated);
-	for (i = 0, i < len; i++)
+	for (i = 0; i < len; i++)
 		printf("Element %d: %s\n", i, "str");
 }
