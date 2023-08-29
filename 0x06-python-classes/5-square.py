23cc83907dc66ecec85856e4263538cae2946f80
Salma_Salah <salma.s.kabel@gmail.com>
2023-08-29 15:30:16 -0700

    answer

--- 0x06-python-classes/5-square.py
+++ 0x06-python-classes/5-square.py
@@ -1,12 +1,14 @@
 #!/usr/bin/python3
+"""Create square class"""
 class Square():
+    """Initialize the class"""
     def __init__(self, size=0):
         self.__size = size
-
+    """Retrieve size"""
     @property
     def size(self):
         return self.__size
-
+    """Set size and value"""
     @size.setter
     def size(self, value):
         if (type(value) is not int):
@@ -14,10 +16,10 @@ class Square():
         elif (value < 0):
             raise ValueError("size must be >= 0")
         self.__size = value
-
+    """Calculate square area"""
     def area(self):
         return(self.__size * self.__size)
-
+    """Print the square"""
     def my_print(self):
         if self.__size > 0:
             for i in range(self.__size):
