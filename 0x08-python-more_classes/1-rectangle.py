cf6da76f2b1171119cbe7b15ffc8bbad4a7f48f3
Salma_Salah <salma.s.kabel@gmail.com>
2023-09-04 13:20:05 -0700

    asnwer

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -5,7 +5,7 @@
 class Rectangle:
     """Initialize the class"""
     def __init__(self, width=0, height=0):
-        self.__width = width
+        self.width = width
         self.height = height
 
     """Retrieve width"""
