2c0cbdb545c318272127b077459081c54477e977
Salma_Salah <salma.s.kabel@gmail.com>
2023-09-12 06:57:04 -0700

    answer

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
 def read_file(filename=""):
     with open(filename, mode='r', encoding="utf-8") as f:
-        rd = f.read()
-        print("{}".format(rd))
+        print(f.read())
