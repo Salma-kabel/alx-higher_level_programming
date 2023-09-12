ca079ee8a1fcb20ba4f4dadeaf71388282c47d97
Salma_Salah <salma.s.kabel@gmail.com>
2023-09-12 06:58:57 -0700

    answer

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,4 +1,7 @@
 #!/usr/bin/python3
+"""function to read text file and prints to stdout"""
+
+
 def read_file(filename=""):
     with open(filename, mode='r', encoding="utf-8") as f:
         print(f.read(), end="")
