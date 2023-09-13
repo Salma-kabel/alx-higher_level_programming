a04bf3c8ef1d3579e014a654ead93c1612261cd6
Salma_Salah <salma.s.kabel@gmail.com>
2023-09-13 08:59:20 -0700

    answer

---
+++ 0x0B-python-input_output/101-stats.py
@@ -0,0 +1,44 @@
+#!/usr/bin/python3
+"""reads stdin line by line and computes metrics"""
+import sys
+
+
+def metrics():
+    """computes metrics"""
+    total_size = 0
+    codes = {}
+    possible_codes = ['200', '301', '400', '401', '403', '404', '405', '500']
+    count = 0
+    try:
+        for line in sys.stdin:
+            if count == 10:
+                print("File size: {}".format(total_size))
+                for key in sorted(codes):
+                    print("{}: {}".format(key, codes[key]))
+                count = 1
+            else:
+                count += 1
+            line = line.split()
+            try:
+                total_size += int(line[-1])
+            except (IndexError, ValueError):
+                pass
+            try:
+                if line[-2] in possible_codes:
+                    if line[-2] in codes.keys():
+                        codes[line[-2]] += 1
+                    else:
+                        codes[line[-2]] = 1
+            except IndexError:
+                pass
+        print("File size: {}".format(total_size))
+        for key in sorted(codes):
+            print("{}: {}".format(key, codes[key]))
+    except KeyboardInterrupt:
+        print("File size: {}".format(total_size))
+        for key in sorted(codes):
+            print("{}: {}".format(key, codes[key]))
+        raise
+
+if __name__ == "__main__":
+    metrics()
