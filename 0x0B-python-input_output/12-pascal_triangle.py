260ab34a1a8272739b21a51a21b6b7e80cc99b97
Salma_Salah <salma.s.kabel@gmail.com>
2023-09-13 07:34:09 -0700

    answer

---
+++ 0x0B-python-input_output/12-pascal_triangle.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""return list of lists of integers representing the Pascal’s triangle"""
+
+
+def pascal_triangle(n):
+    pl_list = []
+    if n <= 0:
+        return pl_list
+    for i in range(1, n + 1):
+        in_list = []
+        for j in range(i):
+            if (j == 0 or j == i - 1):
+                in_list.append(1)
+            else:
+                in_list.append(pl_list[i - 2][j - 1] + pl_list[i - 2][j])
+        pl_list.append(in_list)
+    return pl_list
