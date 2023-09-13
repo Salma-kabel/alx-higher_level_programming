c6bd54d2479ee8c6d3ea3a7eee441e40fddf6612
Salma_Salah <salma.s.kabel@gmail.com>
2023-09-13 06:39:37 -0700

    answer

--- 0x0B-python-input_output/10-student.py
+++ 0x0B-python-input_output/10-student.py
@@ -1,21 +1,24 @@
 #!/usr/bin/python3
-"""Defines a class Student."""
+"""defines class Student"""
 
 
-class Student:
-    """initialisation"""
+class Student():
+    """class to define student"""
+
     def __init__(self, first_name, last_name, age):
+        """initializes student instances"""
         self.first_name = first_name
         self.last_name = last_name
         self.age = age
 
-    """ retrieves a dictionary representation"""
     def to_json(self, attrs=None):
-        if type(attrs) == list:
-            dict_ = {}
-            for atrr in attrs:
-                if hasattr(self, atrr):
-                    dict_[attr] = self.__dict__[attr]
-            return dict_
-
-        return self.__dict__
+        """returns dictionary descript for JSON serialization"""
+        if type(attrs) is list:
+            new_dict = {}
+            for key in self.__dict__:
+                for key2 in attrs:
+                    if key == key2:
+                        new_dict[key] = self.__dict__[key]
+            return new_dict
+        else:
+            return self.__dict__
