6b85f66bafcaf8339134954168c316dc1d244110
Salma_Salah <salma.s.kabel@gmail.com>
2023-09-18 11:07:22 -0700

    answer

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -24,3 +24,28 @@ class Square(Rectangle):
         """set value for width and height"""
         self.width = value
         self.height = value
+        
+    def update(self, *args, **kwargs):
+        """assigns an argument to each attribute"""
+        if args is not None and len(args) != 0:
+            for i in range(len(args)):
+                if i == 0:
+                    self.id = args[0]
+                if i == 1:
+                    self.width = args[1]
+                    self.height = args[1]
+                if i == 2:
+                    self.x = args[3]
+                if i == 3:
+                    self.y = args[4]
+        else:
+            for key in kwargs:
+                if key == "id":
+                    self.id = (kwargs[key])
+                if key == "size":
+                    self.width = (kwargs[key])
+                    self.height = (kwargs[key])
+                if key == "x":
+                    self.x = (kwargs[key])
+                if key == "y":
+                    self.y = (kwargs[key])
