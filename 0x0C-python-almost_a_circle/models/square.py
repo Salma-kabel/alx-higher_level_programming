9dbaf3b3a868128e7986641bebaa00052b195597
Salma_Salah <salma.s.kabel@gmail.com>
2023-09-18 10:45:02 -0700

    answer

---
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""class square iherits from class rectangle"""
+from models.rectangle import Rectangle
+
+
+class Square(Rectangle):
+    """initialization of square class"""
+    def __init__(self, size, x=0, y=0, id=None):
+        super().__init__(size, size, x, y, id)
+
+    def __str__(self):
+        """ return [Square] (<id>) <x>/<y> - <size>"""
+        str1 = "[Square] ({}) {}/{} - {}".format(
+            self.id, self.x, self.y, self.width)
+        return str1
+
+    @property
+    def size(self):
+        """retrieves size attribute"""
+        return self.width
+
+    @size.setter
+    def size(Self, value):
+        """set value for width and height"""
+        self.width = value
+        self.height = value
