5cc69274f03e38ef003b4fff9b00c210e131580d
Salma_Salah <salma.s.kabel@gmail.com>
2023-09-18 09:06:52 -0700

    answer

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -84,6 +84,8 @@ class Rectangle(Base):
 
     def __str__(self):
         """ returns [Rectangle] (<id>) <x>/<y> - <width>/<height>"""
-        str1 = "[Rectangle] {} {}/{} - {}/{}".format(self.id , self.x, self.y, self.width, self.height)
+        str1 = "[Rectangle] ({}) {}/{} - {}/{}".format(
+            str(self.id), str(self.x), str(self.y),
+            str(self.width), str(self.height))
 
         return str1
