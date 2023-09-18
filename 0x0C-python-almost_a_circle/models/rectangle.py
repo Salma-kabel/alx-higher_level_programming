bff9bfa6f0f85de6d51442273cc0bfc1ce6867e3
Salma_Salah <salma.s.kabel@gmail.com>
2023-09-18 09:42:17 -0700

    answer

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -92,16 +92,29 @@ class Rectangle(Base):
 
         return string
 
-    def update(self, *args):
+    def update(self, *args, **kwargs):
         """assigns an argument to each attribute"""
-        for i in range(len(args)):
-            if i == 0:
-                self.id = args[0]
-            if i == 1:
-                self.width = args[1]
-            if i == 2:
-                self.height = args[2]
-            if i == 3:
-                self.x = args[3]
-            if i == 4:
-                self.y = args[4]
+        if args is not None and len(args) != 0:
+            for i in range(len(args)):
+                if i == 0:
+                    self.id = args[0]
+                if i == 1:
+                    self.width = args[1]
+                if i == 2:
+                    self.height = args[2]
+                if i == 3:
+                    self.x = args[3]
+                if i == 4:
+                    self.y = args[4]
+        else:
+            for key in kwargs:
+                if kw == "id":
+                    self.id = (kwargs[key])
+                if kw == "width":
+                    self.width = (kwargs[key])
+                if kw == "height":
+                    self.height = (kwargs[key])
+                if kw == "x":
+                    self.x = (kwargs[key])
+                if kw == "y":
+                    self.y = (kwargs[key])
