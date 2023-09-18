7781b3ce9203263a540a8f06105de47a9c0d55f7
Salma_Salah <salma.s.kabel@gmail.com>
2023-09-18 11:49:28 -0700

    answer

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 """create a class base class will be the “base” of all other classes"""
+import json
 
 
 class Base():
@@ -13,3 +14,35 @@ class Base():
         else:
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
+
+    @staticmethod
+    def to_json_string(list_dictionaries):
+        """returns the JSON string representation of list_dictionaries"""
+        if list_dictionaries is None or len(list_dictionaries) == 0:
+            return "[]"
+        else:
+            return json.dumps(list_dictionaries)
+
+    @classmethod
+    def save_to_file(cls, list_objs):
+        """writes the JSON string representation of list_objs to a file"""
+        filename = cls.__name__ + ".json"
+        list1 = []
+        if list_objs is None:
+            pass
+        else:
+            for i in range(len(list_objs)):
+                list1.append(list_objs[i].to_dictionary())
+        list2 = cls.to_json_string(list1)
+
+        with open(filename, mode='w', encoding="utf-8") as f:
+            f.write(list2)
+
+    def from_json_string(json_string):
+         """returns the list of the JSON string representation json_string"""
+         list1 = []
+         if jsin_string is None or len(json_string) == 0:
+             return list1
+         list1.append(json.loads(json_string))
+         return list1
+
