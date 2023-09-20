a5b205b578c109b2e74625555ae51d6124a44386
Salma_Salah <salma.s.kabel@gmail.com>
2023-09-20 07:59:36 -0700

    answer

--- 0x0C-python-almost_a_circle/tests/test_models/test_rectangle.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_rectangle.py
@@ -6,10 +6,11 @@ from models.rectangle import Rectangle
 """test for rectangle class"""
 
 
-class TestRectangle(unittest.Testcase):
+class TestRectangle(unittest.TestCase):
     """test for every method"""
     def setUp(self):
         """create an instance before any test"""
+        Base._Base__nb_objects = 0
 
     def test_init(self):
         """test for inistantiation function"""
@@ -17,19 +18,19 @@ class TestRectangle(unittest.Testcase):
         self.assertEqual(rect1.width, 4)
         self.assertEqual(rect1.height, 3)
         self.assertEqual(rect1.x, 0)
-        self.assertEqual(rect1,y, 0)
+        self.assertEqual(rect1.y, 0)
         self.assertEqual(rect1.id, 1)
         rect2 = Rectangle(10, 4, 2, 1, 50)
         self.assertEqual(rect2.width, 10)
         self.assertEqual(rect2.height, 4)
         self.assertEqual(rect2.x, 2)
-        self.assertEqual(rect2,y, 1)
+        self.assertEqual(rect2.y, 1)
         self.assertEqual(rect2.id, 50)
         rect3 = Rectangle(20, 8, 1)
         self.assertEqual(rect3.width, 20)
         self.assertEqual(rect3.height, 8)
         self.assertEqual(rect3.x, 1)
-        self.assertEqual(rect3,y, 0)
+        self.assertEqual(rect3.y, 0)
         self.assertEqual(rect3.id, 2)
 
 
