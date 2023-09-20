90026165a190a4d2694cb9bb98481f54223b291b
Salma_Salah <salma.s.kabel@gmail.com>
2023-09-20 06:08:26 -0700

    answer

--- 0x0C-python-almost_a_circle/tests/test_models/test_base.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_base.py
@@ -19,3 +19,19 @@ class TestBase(unittest.TestCase):
         self.assertEqual(base2.id, 89)
         base3 = Base()
         self.assertEqual(base3.id, 2)
+
+    def test_tojsonstring(self):
+        base1 = Base()
+        self.assertEqual(base1.to_json_string(None), "[]")
+        self.assertEqual(base1.to_json_string([]), "[]")
+        res3 = base1.to_json_string([{'test1' : 1, 'test2' : None}])
+        self.assertEqual(res3, '[{"test1": 1, "test2": null}]')
+
+    def test_pep8(self):
+        """test that code follows pep8 style guidelines"""
+        pep8style = pep8.StyleGuide(quiet=True)
+        result = pep8style.check_files(['models/base.py',
+                                        'models/rectangle.py',
+                                        'models/square.py'])
+        self.assertEqual(result.total_errors, 0,
+                         "Found code style errors.")
