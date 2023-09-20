a5320053d5d28c60f5b68a4dd1758fe9b124b4fe
Salma_Salah <salma.s.kabel@gmail.com>
2023-09-20 05:25:29 -0700

    answer

--- 0x0C-python-almost_a_circle/tests/test_models/test_base.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_base.py
@@ -5,15 +5,15 @@ from models.base import Base
 from models.square import Square
 from models.rectangle import Rectangle
 from io import StringIO
-from unittest import TestCase
 from unittest.mock import patch
 
-
-def test_id(unittest.TestCase):
-    """ Test id attribute"""
-    base1 = base()
-    self.assertEqual(base1.id, 1)
-    base2 = base(5)
-    self.assertEqual(base2.id, 5)
-    base3 = base()
-    self.assertEqual(base1.id, 3)
+class TestBase(unittest.TestCase):
+    """test id attribute"""
+    def test_id(self):
+        """ Test id attribute"""
+        base1 = Base()
+        self.assertEqual(base1.id, 1)
+        base2 = Base(5)
+        self.assertEqual(base2.id, 5)
+        base3 = Base()
+        self.assertEqual(base3.id, 2)
