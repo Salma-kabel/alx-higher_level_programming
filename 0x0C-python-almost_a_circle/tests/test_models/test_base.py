6d8048bb588ff61f90693b9b684908777028e713
Salma_Salah <salma.s.kabel@gmail.com>
2023-09-20 05:36:36 -0700

    answer

--- 0x0C-python-almost_a_circle/tests/test_models/test_base.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_base.py
@@ -6,6 +6,8 @@ from models.square import Square
 from models.rectangle import Rectangle
 from io import StringIO
 from unittest.mock import patch
+import pep8
+
 
 class TestBase(unittest.TestCase):
     """test id attribute"""
@@ -17,3 +19,13 @@ class TestBase(unittest.TestCase):
         self.assertEqual(base2.id, 89)
         base3 = Base()
         self.assertEqual(base3.id, 2)
+
+
+    def test_pep8(self):
+        """test that code follows pep8 style guidelines"""
+        pep8style = pep8.StyleGuide(quiet=True)
+        result = pep8style.check_files(['models/base.py',
+                                        'models/rectangle.py',
+                                        'models/square.py'])
+        self.assertEqual(result.total_errors, 0,
+                         "Found code style errors (and warnings).")
