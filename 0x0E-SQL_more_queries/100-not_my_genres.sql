1e6a53fcaf664c1ba16725ef89a75cafa16e305b
Salma_Salah <salma.s.kabel@gmail.com>
2023-10-18 14:00:47 -0700

    answer

--- 0x0E-SQL_more_queries/100-not_my_genres.sql
+++ 0x0E-SQL_more_queries/100-not_my_genres.sql
@@ -3,4 +3,4 @@ SELECT name FROM tv_genres WHERE id NOT IN(SELECT tv_genres.id FROM tv_genres IN
 ON tv_genres.id = tv_show_genres.genre_id INNER JOIN tv_shows
 ON tv_show_genres.show_id = tv_shows.id
 WHERE tv_shows.title = "Dexter"
-ORDER BY tv_genres.name;)
+ORDER BY tv_genres.name);
