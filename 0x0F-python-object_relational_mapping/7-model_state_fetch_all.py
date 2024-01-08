e5bdffb3aca9c300470737d16704ceb8389918a2
Salma_Salah <salma.s.kabel@gmail.com>
2024-01-08 11:06:52 -0800

    answer

--- 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
+++ 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -4,10 +4,10 @@ lists all State objects from the database hbtn_0e_6_usa
 """
 
 
-from model_state import Base, State
 import sys
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
+from model_state import Base, State
 
 
 if __name__ == "__main__":
