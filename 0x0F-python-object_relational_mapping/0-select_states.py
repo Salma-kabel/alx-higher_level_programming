1cbe6d80babb46abf1f1dcbdbd546dcec3b65054
Salma_Salah <salma.s.kabel@gmail.com>
2024-01-08 05:36:53 -0800

    answer

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -10,6 +10,8 @@ database hbtn_0e_0_usa
 
 
 if __name__ == "__main__":
+    """connect to data base"""
+
     db = MySQLdb.connect(host="localhost",
                          user=argv[1], passwd=argv[2], db=argv[3])
     cur = db.cursor()
