f4def86625f14e689cf0172aeb7fad8143e3ec07
Salma_Salah <salma.s.kabel@gmail.com>
2024-01-08 13:27:47 -0800

    answer

--- 0x0F-python-object_relational_mapping/model_city.py
+++ 0x0F-python-object_relational_mapping/model_city.py
@@ -13,6 +13,6 @@ class City(Base):
     links to the MySQL table cities
     """
     __tablename__ = "cities"
-    id = Column(Integer, unique=True, nullable=False,primary_key=True)
+    id = Column(Integer, unique=True, nullable=False, primary_key=True)
     name = Column(String(128), nullable=False)
     state_id = Column(Integer, ForeignKey("states.id"), nullable=False)
