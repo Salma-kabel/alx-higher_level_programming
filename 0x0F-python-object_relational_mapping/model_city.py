0f1a44c23c2d3de77667f55f8d20ee3121d607f4
Salma_Salah <salma.s.kabel@gmail.com>
2024-01-08 13:14:36 -0800

    answer

--- 0x0F-python-object_relational_mapping/model_city.py
+++ 0x0F-python-object_relational_mapping/model_city.py
@@ -4,10 +4,16 @@ contains the class definition of a City
 that inherits instance Base = declarative_base()
 """
 
-
+"""
 from sqlalchemy import Column, Integer, String, ForeignKey
 from model_state import Base
-
+"""
+from lib2to3.pytree import Base
+from sre_parse import State
+from unicodedata import name
+from sqlalchemy import Column, ForeignKey, Integer, String, null
+from sqlalchemy.ext.declarative import declarative_base
+Base = declarative_base()
 
 class City(Base):
     """inherits from base links to the MySQL table cities"""
