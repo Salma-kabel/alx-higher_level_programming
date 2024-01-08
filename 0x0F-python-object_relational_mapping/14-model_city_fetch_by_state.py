a44d1862ef49e7c7686eb0e0209476fb6c5a77ed
Salma_Salah <salma.s.kabel@gmail.com>
2024-01-08 13:06:21 -0800

    answer

--- 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
+++ 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
@@ -1,22 +1,22 @@
 #!/usr/bin/python3
 """
-lists all cities from database by state in ascending order by id
-using SQLAlchemy and importing State and Base from model_state
+prints all City objects from the database hbtn_0e_14_usa
 """
+
+
 from sys import argv
-from model_state import Base, State
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from model_state import State
 from model_city import City
 
-from sqlalchemy import (create_engine)
-from sqlalchemy.orm import sessionmaker
+
 
 if __name__ == "__main__":
-        engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(
-            argv[1], argv[2], argv[3]), pool_pre_ping=True)
-        Base.metadata.create_all(engine)
-        Session = sessionmaker(bind=engine)
-        session = Session()
-        for instance in session.query(City, State).join(
-                        State, City.state_id == State.id).order_by(City.id):
-                print("{}: ({}) {}".format(
-                        instance[1].name, instance[0].id, instance[0].name))
+    engine = create_engine("mysql+mysqldb://{}:{}@localhost/{}".format(
+        argv[1], argv[2], argv[3]), pool_pre_ping=True)
+    session1 = sessionmaker(bind=engine)
+    session = session1()
+    for state, city in session.query(State, City).filter(
+            State.id == City.state_id).order_by(City.id):
+                print("{}: ({}) {}".format(state.name, city.id, city.name))
