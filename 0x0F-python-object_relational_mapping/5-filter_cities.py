f496c22724ad68387b370901aa782118abf84026
Salma_Salah <salma.s.kabel@gmail.com>
2024-01-08 08:18:48 -0800

    answer

--- 0x0F-python-object_relational_mapping/5-filter_cities.py
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -11,10 +11,10 @@ if __name__ == "__main__":
     db = MySQLdb.connect(host="localhost",
                          user=argv[1], passwd=argv[2], db=argv[3])
     cur = db.cursor()
-    cur.execute("""SELECT cities.name FROM cities
+    cur.execute("""SELECT cities.name, states.name FROM cities
                 JOIN states ON cities.state_id = states.id
                 ORDER BY cities.id""")
     states = cur.fetchall()
     for state in states:
-        if state[2] == argv[4]:
-            print(state)
+        if state[1] == argv[4]:
+            print(state[0])
