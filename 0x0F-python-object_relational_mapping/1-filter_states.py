214af3e577855c2247d5aeba1a8fc9936e220545
Salma_Salah <salma.s.kabel@gmail.com>
2024-01-08 06:01:56 -0800

    answer

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -10,7 +10,7 @@ import MySQLdb
 
 def main():
     """
-    connect to database
+    connect to database Usage: ./1-filter_states.py <mysql username>
     """
     db = MySQLdb.connect(host="localhost", user=argv[1], passwd=argv[2], db=argv[3])
     cur = db.cursor()
