80bec0b1f1ac30a793bc7eeee87c075911ecf412
Salma_Salah <salma.s.kabel@gmail.com>
2024-01-09 04:06:50 -0800

    answer

--- 0x0F-python-object_relational_mapping/relationship_city.py
+++ 0x0F-python-object_relational_mapping/relationship_city.py
@@ -11,6 +11,6 @@ from relationship_state import Base
 class City(Base):
     """inherits from base"""
     __tablename__ = "cities"
-    id = Column(Integer, unique=True, nullable=False,primary_key=True)
+    id = Column(Integer, unique=True, nullable=False, primary_key=True)
     name = Column(String(128), nullable=False)
     state_id = Column(Integer, ForeignKey('states.id'), nullable=False)
