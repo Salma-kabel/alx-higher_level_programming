8f71065c67d9287975612ee18b530f3c4b1af543
Salma_Salah <salma.s.kabel@gmail.com>
2024-01-09 06:05:06 -0800

    answer

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -11,15 +11,15 @@ def find_peak(list_of_integers):
     """
     if list_of_integers is None or list_of_integers == []:
         return None
-    if len(list_of_integers) == 1:
+    lth = len(list_of_integers)
+    if lth == 1:
         return (list_of_integers[0])
-    mid = int(len(list_of_integers) / 2)
-    if list_of_integers[mid] > list_of_integers[mid - 1] and \
-            list_of_integers[mid] > list_of_integers[mid + 1]:
-        return list_of_integers[mid]
+    mid = int(lth / 2)
+    if mid - 1 > 0 and list_of_integers[mid] > list_of_integers[mid - 1]:
+        if mid + 1 < lth and list_of_integers[mid] > list_of_integers[mid + 1]:
+            return list_of_integers[mid]
     num = find_peak(list_of_integers[mid:])
     if num is not None:
         return num
     else:
         return find_peak(list_of_integers[:mid])
-                
