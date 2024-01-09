8ad2ef0617b6ee9f3858728bd8e06cd76dce7b7c
Salma_Salah <salma.s.kabel@gmail.com>
2024-01-09 05:50:23 -0800

    answ2

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -6,6 +6,9 @@ finds a peak in a list of unsorted integers.
 
 
 def find_peak(list_of_integers):
+    """
+    finds a peak in a list of unsorted ints
+    """
     if list_of_integers is None or list_of_integers == []:
         return None
     if len(list_of_integers) == 1:
