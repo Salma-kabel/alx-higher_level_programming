eb913c056b0800a9ddeebb14fc28c31435a5fa14
Salma_Salah <salma.s.kabel@gmail.com>
2024-01-10 12:02:08 -0800

    answer

---
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -0,0 +1,13 @@
+#!/bin/bash
+#script that fetches https://alx-intranet.hbtn.io/status
+
+import urllib.request
+
+
+if __name__ == "__main__":
+    with urllib.request.urlopen('https://alx-intranet.hbtn.io/status') as response:
+        html = response.read()
+        print("Body response:")
+        print("\t- type: {}".format(type(response)))
+        print("\t- content: {}".format(response))
+        print("\t- utf8 content: {}".format(response.decode(encoding='utf-8')))
