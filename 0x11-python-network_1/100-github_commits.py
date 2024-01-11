fba36440694616cc553f412b9758ccd71a1c7502
Salma_Salah <salma.s.kabel@gmail.com>
2024-01-11 07:39:21 -0800

    answer

--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -13,7 +13,7 @@ if __name__ == "__main__":
     url = "https://api.github.com/repos/{}/{}/commits".format(
         argv[2], argv[1])
     r = requests.get(url)
-    lis1 = r.json()
+    list1 = r.json()
     i = 0
     for commit in list1:
         if i == 10:
