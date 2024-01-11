bf842edb94acccdd11795eb926b37b8027aedc54
Salma_Salah <salma.s.kabel@gmail.com>
2024-01-11 07:35:59 -0800

    answer

---
+++ 0x11-python-network_1/100-github_commits.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""
+script that takes 2 arguments
+in order to solve this challenge
+"""
+
+
+import requests
+from sys import argv
+
+
+if __name__ == "__main__":
+    url = "https://api.github.com/repos/{}/{}/commits".format(
+        sys.argv[2], sys.argv[1])
+    r = requests.get(url)
+    lis1 = r.json()
+    i = 0
+    for commit in list1:
+        if i == 10:
+            break
+        print("{}: {}".format(
+            commit.get('sha'),
+            commit.get('commit').get('author').get('name')))
+        i = i + 1
