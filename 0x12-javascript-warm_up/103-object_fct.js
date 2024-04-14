2ea907b3564d3c2b1ab8feb57d155b0a04cf81d4
Salma_Salah <salma.s.kabel@gmail.com>
2024-04-14 14:47:34 -0700

    task16

--- 0x12-javascript-warm_up/103-object_fct.js
+++ 0x12-javascript-warm_up/103-object_fct.js
@@ -5,8 +5,8 @@ const myObject = {
 };
 console.log(myObject);
 myObject.incr = function () {
-  mtObject.value++;
-}
+  myObject.value++;
+};
 myObject.incr();
 console.log(myObject);
 myObject.incr();
