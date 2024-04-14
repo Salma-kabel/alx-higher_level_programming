eb2c14f6182fedb765e1af368b9d926cf1014183
Salma_Salah <salma.s.kabel@gmail.com>
2024-04-14 06:34:00 -0700

    task9

---
+++ 0x12-javascript-warm_up/9-add.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+const args = process.argv;
+function add(a, b) {
+  return a + b;
+}
+const res = add(Number(args[2]), Number(args[3]));
+if (!isNaN(res))) {
+  console.log(res);
+} else {
+  console.log('NaN');
+}
