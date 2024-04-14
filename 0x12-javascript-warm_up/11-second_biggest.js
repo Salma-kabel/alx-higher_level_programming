77148db68571ebbeeeb64c5387c6b6a7f2b7e41d
Salma_Salah <salma.s.kabel@gmail.com>
2024-04-14 14:08:27 -0700

    task11

---
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -0,0 +1,25 @@
+#!/usr/bin/node
+const args = process.argv;
+if (args.length < 4) {
+  console.log(0);
+} else {
+  let s;
+  let b;
+  let i = 2;
+  if (args[i] > args[i + 1]) {
+    b = args[i];
+    s = args[i + 1];
+  } else {
+    b = args[i + 1];
+    s = args[i];
+  }
+  for (i; i < args.length; i++) {
+    if (args[i] > b) {
+      s = b;
+      b = args[i];
+    } else if (args[i] > s && args[i] < b) {
+      s = args[i];
+    }
+  }
+  console.log(s);
+}
