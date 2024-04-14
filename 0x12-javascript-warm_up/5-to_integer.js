c309aa9393dd16ffab777f11779007dd8e7ba386
Salma_Salah <salma.s.kabel@gmail.com>
2024-04-14 06:03:12 -0700

    task5

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
 const args = process.argv;
-if (typeof(args[2]) === 'number' || typeof(Number(args[2])) === 'number') {
+if (!isNaN(Number(args[2]))) {
   console.log('My number: ' + args[2]);
 } else {
 console.log('Not a number');
