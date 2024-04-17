3707fb85900f2951a607552cf2a1e049645e4596
Salma_Salah <salma.s.kabel@gmail.com>
2024-04-17 10:19:57 -0700

    task2

--- 0x13-javascript_objects_scopes_closures/3-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/3-rectangle.js
@@ -6,7 +6,8 @@ class Rectangle {
       this.height = h;
     }
   }
-  print() {
+
+  print () {
     for (let i = 0; i < this.height; i++) {
       let arr = '';
       for (let j = 0; j < this.width; j++) {
