f31cd95ebfecfa890f4ee0af0be680442bcc4369
Salma_Salah <salma.s.kabel@gmail.com>
2024-04-17 10:24:49 -0700

    task4

---
+++ 0x13-javascript_objects_scopes_closures/4-rectangle.js
@@ -0,0 +1,31 @@
+#!/usr/bin/node
+class Rectangle {
+  constructor (w, h) {
+    if (w > 0 && h > 0) {
+      this.width = w;
+      this.height = h;
+    }
+  }
+
+  print () {
+    for (let i = 0; i < this.height; i++) {
+      let arr = '';
+      for (let j = 0; j < this.width; j++) {
+        arr = arr + 'X';
+      }
+      console.log(arr);
+    }
+  }
+
+  rotate () {
+    let i = this.width;
+    this.width = this.height;
+    this.height = i;
+  }
+
+  double () {
+    this.width = this.width * 2;
+    this.height = this.height * 2;
+  }
+}
+module.exports = Rectangle;
