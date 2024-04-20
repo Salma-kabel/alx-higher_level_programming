24c78d1c059f61bc2cde52d47cb0b3233c59db48
Salma_Salah <salma.s.kabel@gmail.com>
2024-04-20 05:40:15 -0700

    task6

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -6,7 +6,13 @@ class Square extends Squareone {
     if (c === undefined) {
       c = 'X';
     }
-    this.print();
+    for (let i = 0; i < this.height; i++) {
+      let arr = '';
+      for (let j = 0; j < this.width; j++) {
+        arr = arr + c;
+      }
+      console.log(arr);
+    }
   }
 }
 module.exports = Square;
