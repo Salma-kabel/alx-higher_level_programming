91ff125beb87737cfedbbba03f7207580f5eca32
Salma_Salah <salma.s.kabel@gmail.com>
2024-04-21 06:46:34 -0700

    task7

--- 0x13-javascript_objects_scopes_closures/7-occurrences.js
+++ 0x13-javascript_objects_scopes_closures/7-occurrences.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 exports.nbOccurences = function (list, searchElement) {
  let num = 0;
-  for element in list {
+  for (const element of list) {
     if (element === searchElement) {
       num++;
     }
