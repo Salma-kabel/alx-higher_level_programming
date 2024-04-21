f7cfa8bd62ca42c42c264e9817d07d21e8c68ce5
Salma_Salah <salma.s.kabel@gmail.com>
2024-04-21 12:28:45 -0700

    task12

--- 0x13-javascript_objects_scopes_closures/101-sorted.js
+++ 0x13-javascript_objects_scopes_closures/101-sorted.js
@@ -1,8 +1,8 @@
 #!/usr/bin/node
 const dict = require('./101-data.js').dict;
-let dict2 = {}
+const dict2 = {};
 for (const element in dict) {
-  if (!dict2.hasOwnProperty(dict[element])) {
+  if (!(Object.prototype.hasOwnProperty.call(dict2, dict[element]))) {
     dict2[dict[element]] = [element]
   } else {
     dict2[dict[element]].push(element);
