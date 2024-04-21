7bd40817705c1971f1f43b1ac50cf435d8c59990
Salma_Salah <salma.s.kabel@gmail.com>
2024-04-21 10:21:38 -0700

    task8

---
+++ 0x13-javascript_objects_scopes_closures/8-esrever.js
@@ -0,0 +1,9 @@
+#!/usr/bin/node
+exports.esrever = function (list) {
+  const arr[list.length];
+  let i = 0;
+  for (i; i < list.length; i++) {
+    arr[i] = list[list.length - 1 - i];
+  }
+  return arr;
+};
