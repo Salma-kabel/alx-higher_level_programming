31f640651eb8591252281ef041604b5712d7eee8
Salma_Salah <salma.s.kabel@gmail.com>
2024-04-21 11:19:18 -0700

    task9

--- 0x13-javascript_objects_scopes_closures/9-logme.js
+++ 0x13-javascript_objects_scopes_closures/9-logme.js
@@ -1,8 +1,8 @@
 #!/usr/bin/node
-exports.logMe = function (item) {
+exports.logMe = (function (item) {
   let count = 0;
   return function () {
-    count++;
     console.log(count + ': ' + item);
+    count++;
   }
-}
+})();
