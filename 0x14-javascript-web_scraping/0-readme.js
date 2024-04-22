77a21aecfe748940d2cf6fd4bdc3a4165206dcf0
Salma_Salah <salma.s.kabel@gmail.com>
2024-04-22 02:20:08 -0700

    task0

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -2,5 +2,9 @@
 const file = process.argv[2];
 const fs = require('fs');
 fs.readFile(file, 'utf8', function(err, data) {
+  if (err) {
+    console.log(err);
+  } else {
   console.log(data);
+  }
 });
