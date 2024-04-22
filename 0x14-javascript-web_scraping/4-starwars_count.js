28006c476c90d0ea9095153f65f0edf560959344
Salma_Salah <salma.s.kabel@gmail.com>
2024-04-22 03:16:42 -0700

    task4

---
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -0,0 +1,21 @@
+#!/usr/bin/node
+const request = require('request');
+const url = process.argv[2];
+request(url, function (err, response, body) {
+  if (err) {
+    console.log(err);
+  } else {
+    const result = JSON.parse(body).results;
+    const num = 0;
+    for (let i = 0; i < result.length; i++) {
+      const characters = result[i].characters;
+      for (let j = 0; j < characters.length; j++) {
+        if (characters[j].includes('18')) {
+          num++;
+          break;
+        }
+      }
+    }
+    console.log(num);
+  }
+});
