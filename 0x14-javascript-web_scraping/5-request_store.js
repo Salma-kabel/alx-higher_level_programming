5ea8438908f79ceb414561c9d598d53bf6f942da
Salma_Salah <salma.s.kabel@gmail.com>
2024-04-22 03:34:41 -0700

    task5

--- 0x14-javascript-web_scraping/5-request_store.js
+++ 0x14-javascript-web_scraping/5-request_store.js
@@ -3,7 +3,7 @@ const fs = require ('fs');
 const request = require('request');
 const url = process.argv[2];
 const file = process.argv[3];
-request(file, function (err, response, body) {
+request(url, function (err, response, body) {
   if (err) {
     console.log(err);
   } else {
