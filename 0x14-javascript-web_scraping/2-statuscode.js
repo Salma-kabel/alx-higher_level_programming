34f711a836de8d411f2c4ea40d6da374fa654fc1
Salma_Salah <salma.s.kabel@gmail.com>
2024-04-22 02:38:40 -0700

    task2

--- 0x14-javascript-web_scraping/2-statuscode.js
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -1,5 +1,6 @@
 #!/usr/bin/node
-url = process.argv[2];
-const request = new Request(url);
-const response = fetch(request);
+const request = require('request');
+const url = process.argv[2];
+const req = new Request(url);
+const response = fetch(req);
 console.log('code: ' + response.status);
