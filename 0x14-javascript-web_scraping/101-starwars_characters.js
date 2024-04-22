fa985213cbc72086565c8a20aa0224f1093b637d
Salma_Salah <salma.s.kabel@gmail.com>
2024-04-22 04:46:56 -0700

    task8

--- 0x14-javascript-web_scraping/101-starwars_characters.js
+++ 0x14-javascript-web_scraping/101-starwars_characters.js
@@ -1,21 +1,28 @@
 #!/usr/bin/node
 const request = require('request');
 const url = 'https://swapi-api.alx-tools.com/api/films/' + process.argv[2];
+
 request(url, function (err, response, body) {
   if (err) {
     console.log(err);
   } else {
     const characters = JSON.parse(body).characters;
-    for (let i = 0; i < characters.length; i++) {
-      const idlist = characters[i].split('/');
-      const id = idlist[idlist.length - 2];
-      request('https://swapi-api.alx-tools.com/api/people/' + id, function (err, response, body) {
-        if (err) {
-          console.log(err);
-        } else {
-          console.log(JSON.parse(body).name);
-        }
-      });
-    }
+    printNames(characters, 0);
   }
 });
+
+function printNames(characters, i) {
+  if (i >= characters.length) {
+    return;
+  }
+  const idlist = characters[i].split('/');
+  const id = idlist[idlist.length - 2];
+  request('https://swapi-api.alx-tools.com/api/people/' + id, function (err, response, body) {
+    if (err) {
+      console.log(err);
+    } else {
+      console.log(JSON.parse(body).name);
+      printNames(characters, i + 1);
+    }
+  });
+}
